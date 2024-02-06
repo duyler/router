4461e967bffad8ae8495e1c9cd9c5e528dad94ec
Mikhail Ilinsky <mail@milinsky.ru>
2024-02-07 04:26:31 +1000

    Add remove route

--- src/RouteCollection.php
+++ src/RouteCollection.php
@@ -8,22 +8,27 @@ use Duyler\Router\Handler\UrlGenerator;
 
 class RouteCollection
 {
+    /** @var RouteDefinition[]  */
+    private array $routes = [];
+
     public function __construct()
     {
         new Route($this);
         new Url(new UrlGenerator($this));
     }
 
-    /** @var RouteDefinition[]  */
-    private array $routes = [];
-
     public function add(RouteDefinition $definition): void
     {
-        $this->routes[] = $definition;
+        $this->routes[strtolower($definition->getMethod()) . '@' . $definition->getPattern()] = $definition;
     }
 
     public function getRoutes(): array
     {
         return $this->routes;
+    }
+
+    public function remove(string $method, string $pattern): void
+    {
+        unset($this->routes[strtolower($method) . '@' . $pattern]);
     }
 }
